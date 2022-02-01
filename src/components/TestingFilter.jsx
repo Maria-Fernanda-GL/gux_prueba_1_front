c45ab01f192ba780087a4cd1a82be2bda0d8f52d
Maria Guerra <mariafernanda@norte.digital>
2022-01-31 23:32:20 -0400

    Limpiar formulario

--- src/components/TestingFilter.jsx
+++ src/components/TestingFilter.jsx
@@ -1,48 +1,66 @@
-import React from "react";
+import React, {useState}  from "react";
 import { Form, Button } from "react-bootstrap";
 
 
 export function TestingFilter(){
+    const default_filter = {
+        rol: '',
+        patient: '',
+        prevision: '',
+        estado: '',
+        date_start: '',
+        date_end: ''
+    }
+
+    var [filter, setFilter] = useState(default_filter);
+
+    const handleInputChange = (e) => {
+        setFilter({...filter, [e.target.name] : e.target.value})
+    };
+
+    const resetForm = () => {
+        setFilter(default_filter);
+    };
+
     return(
         <Form>
             <Form.Group  className="mb-3" controlId="rol">
                 <Form.Label>Rol</Form.Label>
-                <Form.Control type="text" placeholder="Enter Rol"/>
+                <Form.Control type="text" placeholder="Enter Rol" name="rol" value={filter.rol} onChange={handleInputChange}/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="paciente">
                 <Form.Label>Paciente</Form.Label>
-                <Form.Control type="text" placeholder="Paciente" />
+                <Form.Control type="text" placeholder="Paciente" name="patient" value={filter.patient} onChange={handleInputChange}/>
             </Form.Group>
 
             <Form.Group  className="mb-3" controlId="prevision">
                 <Form.Label>Prevision</Form.Label>
-                <Form.Control type="text" placeholder="Enter Prevision"/>
+                <Form.Control type="text" placeholder="Enter Prevision" name="prevision" value={filter.prevision} onChange={handleInputChange}/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="estado">
                 <Form.Label>Estado</Form.Label>
-                <Form.Control type="text" placeholder="Enter estado" />
+                <Form.Control type="text" placeholder="Enter estado" name="estado" value={filter.estado} onChange={handleInputChange}/>
             </Form.Group>
 
-            <Form.Group  className="mb-3" controlId="date_start">
+            <Form.Group  className="mb-3" controlId="date_start" >
                 <Form.Label>Fecha alta desde</Form.Label>
-                <Form.Control type="date" />
+                <Form.Control type="date" name="date_start" value={filter.date_start} onChange={handleInputChange}/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="date_end">
                 <Form.Label>Fecha alta hasta</Form.Label>
-                <Form.Control type="date" />
+                <Form.Control type="date" name="date_end" value={filter.date_end} onChange={handleInputChange}/>
             </Form.Group>
 
-            <Button variant="primary" type="submit">
+            <Button variant="primary" onClick={resetForm} >
                 Limpiar
             </Button>
 
-            <Button variant="primary" type="submit">
+            <Button variant="primary" >
                 Buscar
             </Button>
         </Form>
     )
-
 }
